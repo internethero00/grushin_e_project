ecdc2fa03780974a1958246c89184f82191a123e
internethero00 <egormix1703@gmail.com>
2025-12-27 16:15:10 +0200

    stack update

---
+++ src/shared/stack/filter.ts
@@ -0,0 +1,8 @@
+export const filters = [
+    { label: 'All', value: 'all' },
+    { label: 'Frontend', value: 'frontend' },
+    { label: 'Backend', value: 'backend' },
+    { label: 'Databases', value: 'database' },
+    { label: 'DevOps', value: 'devops' },
+    { label: 'Tools', value: 'tools' },
+]
