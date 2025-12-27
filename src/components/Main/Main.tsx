981bd0aad0927975466d817aa86e8b2785b85997
internethero00 <egormix1703@gmail.com>
2025-12-27 20:51:33 +0200

    + stack update, projects

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,13 +1,14 @@
 import {Hero} from "@/components/Main/Hero/Hero.tsx";
 import styles from './Main.module.scss'
 import {Stack} from "@/components/Main/Stack/Stack.tsx";
+import {Projects} from "@/components/Main/Projects/Projects.tsx";
 
 export function Main() {
     return (
         <main className={styles.main}>
             <Hero/>
             <Stack/>
-            {/*<Projects/>*/}
+            <Projects/>
         </main>
     )
 }
--- src/components/Main/Stack/Stack.tsx
+++ src/components/Main/Stack/Stack.tsx
@@ -1,7 +1,16 @@
 import styles from './Stack.module.scss'
 import {Typewriter} from "react-simple-typewriter";
+import {filters} from "@/shared/stack/filter.ts";
+import {useState} from "react";
+import {techStack} from "@/shared/stack/techStack.ts";
 
 export function Stack() {
+
+    const [activeFilter, setActiveFilter] = useState<string>('all')
+    const filterStack = () => {
+        return activeFilter === 'all' ? techStack :
+            techStack.filter(stack => stack.category === activeFilter)
+    }
     return (
         <div className={styles.stack_main}>
             <h1>
@@ -11,13 +20,21 @@ export function Stack() {
                     typeSpeed={70}
                 />
             </h1>
-            <div className={styles.backend}>
-                <h2>Backend</h2>
-                <div className={styles.backend_img}>
-                    <img src={'/back-end/node.svg'} alt={'node.js'}/>
-                    <img src={'/back-end/nest.svg'} alt={'nest.js'}/>
-                    <img src={'/back-end/express.svg'} alt={'express.js'}/>
-                </div>
+            <div className={styles.header_stack}>
+                {filters.map(filter => (
+                    <div
+                        onClick={() => setActiveFilter(filter.value)}
+                        className={`${styles.stack_value} ${activeFilter === filter.value ? styles.active_stack_value : ''}`}
+                        key={filter.value}>{filter.label}</div>
+                ))}
+            </div>
+            <div className={styles.stack_content}>
+                {filterStack().map(stack => (
+                    <div className={styles.stack_icon}>
+                        <img src={stack.icon} alt={stack.name}/>
+                        <p>{stack.name}</p>
+                    </div>
+                ))}
             </div>
         </div>
     )
